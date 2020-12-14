d09147d4139aa3138fdb306cf9b97f2e133a963f
Boomer9955 <73993803+Boomer9955@users.noreply.github.com>
2020-12-14 18:28:24 +0300

    Update jenkins_file.groovy

--- jenkins_file.groovy
+++ jenkins_file.groovy
@@ -1,9 +1,10 @@
 pipeline {
     agent any
+
     stages {
-        stage('Test') {
+        stage('Hello') {
             steps {
-                sh 'hello'
+                echo 'Hello World'
             }
         }
     }
