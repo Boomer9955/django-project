9ce9047ac56d7dd64a94b8103ae9ff8f72e3966b
Boomer9955 <73993803+Boomer9955@users.noreply.github.com>
2020-12-30 13:52:49 +0300

    Update jenkins_file.groovy

--- jenkins_file.groovy
+++ jenkins_file.groovy
@@ -10,7 +10,7 @@ pipeline {
             steps {
                 echo "------Start Build------"
                 dir ('docker'){
-                    sh 'docker build .'
+                    sh 'docker images'
                 }
             }
         }
