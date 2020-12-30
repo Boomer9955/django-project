9471a25284fece4872ea6eedc1304689289ba912
Boomer9955 <73993803+Boomer9955@users.noreply.github.com>
2020-12-30 14:07:48 +0300

    Update jenkins_file.groovy

--- jenkins_file.groovy
+++ jenkins_file.groovy
@@ -10,7 +10,7 @@ pipeline {
             steps {
                 echo "------Start Build------"
                 dir ('docker'){
-                    sh 'sudo yum-config-manager \ --add-repo \ https://download.docker.com/linux/centos/docker-ce.repo'
+                    sh 'sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo'
                     sh 'sudo yum install docker-ce'
                     sh 'sudo systemctl start docker'
                     sh 'sudo docker run hello-world'
