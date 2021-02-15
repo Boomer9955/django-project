55b51b0293c9c97ec7dfe8b850824a548a933964
Boomer9955 <73993803+Boomer9955@users.noreply.github.com>
2021-02-15 12:51:12 +0300

    Delete creat_user.py

--- django/creat_user.py
+++
@@ -1,3 +0,0 @@
-echo "from django.contrib.auth.models import User" > createadmin.py
-echo "User.objects.create_superuser('admin', 'admin@email.io', 'admin')" >> createadmin.py
-python manage.py shell < createadmin.py
