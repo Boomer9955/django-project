19372734d0ce931c87a7a257bfa640a488e98447
Yurkin Roman <rombich@bk.ru>
2021-03-03 18:08:47 +0300

    redjerect

--- django/mydjango/settings.py
+++ django/mydjango/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = '_gi)chhhag$4&-^9+t_5m8qt=@&l1ed55bv$&n*fs8ekr$(@tp'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
