0a317e85b7837790f5672ca8298004525cc7aeb9
Boomer9955 <73993803+Boomer9955@users.noreply.github.com>
2021-04-06 13:14:18 +0300

    Update creat_django.groovy

--- creat_django.groovy
+++ creat_django.groovy
@@ -6,5 +6,5 @@ node {
     def mb = (curDate =~ "\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}")
         ip = mb[0]
         println "${ip}"
-    def res = sh(script: 'curl http://localhost:8000', returnStdout: true)
+    def res = sh(script: 'curl --insecure -u nnarayan1 http://${ip}:8000', returnStdout: true)
 }
