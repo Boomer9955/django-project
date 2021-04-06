7034eb86086d9beb9522595d9e2895f2142836c1
Boomer9955 <73993803+Boomer9955@users.noreply.github.com>
2021-04-06 12:57:06 +0300

    Update creat_django.groovy

--- creat_django.groovy
+++ creat_django.groovy
@@ -1,6 +1,6 @@
 node {
     checkout scm
     curDate = readFile 'ansible/hosts.yml'
-    def match = curDate =~ '([0-9]{1,3}[\\.]){3}[0-9]{1,3}'
-        println match[0]
+    def mb = (curDate =~ /#(\d+)/)
+        println mc[0][1]
 }
