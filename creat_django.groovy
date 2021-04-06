aa8f4b64a194e1402add13d2f0564af3300110fc
Boomer9955 <73993803+Boomer9955@users.noreply.github.com>
2021-04-06 12:59:08 +0300

    Update creat_django.groovy

--- creat_django.groovy
+++ creat_django.groovy
@@ -1,6 +1,8 @@
 node {
-    checkout scm
-    curDate = readFile 'ansible/hosts.yml'
-    def mb = (curDate =~ [0-9])
-        println mb[0]
+    url = 'https://10.11.12.13/some_name'
+    def match = url =~ '^https?://([^/]*)/([^/]*)$'
+        println match
+        println match[0]
+        println match[0][1]
+        println match[0][2]
 }
