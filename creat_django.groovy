77b99cd2750610c3a115d90bd89695f8b044fbb7
Boomer9955 <73993803+Boomer9955@users.noreply.github.com>
2021-04-06 12:48:38 +0300

    Update creat_django.groovy

--- creat_django.groovy
+++ creat_django.groovy
@@ -1,6 +1,5 @@
 node {
     checkout scm
-    def file = new File('ansible/hosts.yml')
-    def yourData = file.eachLine { line ->
-        println line }
+    curDate = readFile 'ansible/hosts.yml'
+     echo "${curDate}"
 }
