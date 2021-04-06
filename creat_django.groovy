6b15e8354889f39d0277f51b704371357be849cd
Boomer9955 <73993803+Boomer9955@users.noreply.github.com>
2021-04-06 11:43:56 +0300

    Update creat_django.groovy

--- creat_django.groovy
+++ creat_django.groovy
@@ -1,8 +1,9 @@
 node {
     checkout scm
+    def hosts = ansible/hosts.yml
     sh "pwd"
     sh "ls"
-    def ip = sh 'grep -E -o "([0-9]{1,3}[\\.]){3}[0-9]{1,3}" hosts.yml'
+    def ip = sh 'grep -E -o "([0-9]{1,3}[\\.]){3}[0-9]{1,3}" ${hosts}'
     println "${ip}"
     sh "curl http://${ip}:8080"
 }
