858c83077b2f3e1e5ecd5a861f1fe82df8bb063e
Boomer9955 <73993803+Boomer9955@users.noreply.github.com>
2021-04-06 12:27:12 +0300

    Update creat_django.groovy

--- creat_django.groovy
+++ creat_django.groovy
@@ -1,9 +1,6 @@
-import org.yaml.snakeyaml.Yaml
 node {
     checkout scm
 
-    Yaml parser = new Yaml()
-    List example = parser.load(("ansible/hosts.yaml" as File).text)
-
-    example.each{println it.subject}
+    Yaml configFileYml = new Yaml("ansible/hosts.yml")
+    configFileYml.load(configFile).each { host -> ... }
 }
