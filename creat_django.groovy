86b8ba776cbaf8e76eb5d97f075aa9cf93c73786
Boomer9955 <73993803+Boomer9955@users.noreply.github.com>
2021-04-06 12:29:11 +0300

    Update creat_django.groovy

--- creat_django.groovy
+++ creat_django.groovy
@@ -1,6 +1,36 @@
 node {
     checkout scm
 
-    Yaml configFileYml = new Yaml("ansible/hosts.yml")
-    configFileYml.load(configFile).each { host -> ... }
+    def dataList = [:]
+    def theInfoName = "ansible/hosts.yml"
+
+    File theInfoFile = new File(theInfoName)
+
+    def words
+    def key
+    def value
+
+    if (!theInfoFile.exists()) {
+         println "File does not exist"
+
+    } else {
+
+     theInfoFile.eachLine { line ->
+
+     if (line.trim().size() == 0) {
+      return null
+
+     } else {
+
+      words = line.split("=")
+      key=words[0] 
+      value=words[1]
+      dataList[key]=value
+
+      println "${words[0]}=${words[1]}"
+      }
+
+     }
+     println "$dataList.Performance"  //test if Performance has over-written the previous Performance value
+    }
 }
