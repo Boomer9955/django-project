00b4de017a0aef87ad99969d597f15e0d1d56d06
Roman <rombich@bk.ru>
2021-06-03 16:56:40 +0300

    s

--- creat_django.groovy
+++ creat_django.groovy
@@ -12,9 +12,8 @@ node{
         def ipad = (curDate =~ "\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}")
                 ip = ipad[0]
             println "${ip}"
-        def us =(curDate =~ "user")
-            u = us[1]
-            println "${u}"
+        def config = new YamlSlurper().parseText(curDate)
+            println "${config.user}"
             //sh "ansible-vault decrypt --vault-password-file {$password} ansible/password.conf"
             //dockerImage = docker.build registry + ":latest"
     }
