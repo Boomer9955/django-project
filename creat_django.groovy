9a811f73b0c4e87b26eacc99a416a277e5230a45
Roman <rombich@bk.ru>
2021-06-03 16:06:57 +0300

    pipe

--- creat_django.groovy
+++ creat_django.groovy
@@ -7,7 +7,7 @@ node{
         checkout scm
     }
     stage('собираем image'){
-        withCredentials([string(credentialsId: 'dockerhub', variable: 'password')]) {
+        withCredentials([string(credentialsId: 'docker_hub', variable: 'password')]) {
             sh "ansible-vault decrypt --vault-password-file {$password} ansible/password.conf"
             sh "cat ansible/password.conf"
             //dockerImage = docker.build registry + ":latest"
