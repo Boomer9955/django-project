0cf561f780d5e2ae3d226fb3cca2a702ba1a978e
Roman <rombich@bk.ru>
2021-06-08 17:18:16 +0300

    _

--- creat_django.groovy
+++ creat_django.groovy
@@ -2,19 +2,16 @@
 //groovy.grape.Grape.grab(group:'org.springframework', module:'spring', version:'2.5.6')
 //@Grab(group='yaml', module='YamlSlurper', version='1.0.1')
 //import groovy.yaml.YamlSlurper
-@Grab(group='org.codehaus.groovy.modules.http-builder', module='http-builder', version='0.7' )
+@Grab(group='org.codehaus.groovy.modules.http-builder', module='http-builder', version='0.7')
 @Grab('org.apache.httpcomponents:httpclient:4.2.6')
-env.registry = "boomer9955/mydjango"
-env.registryCredential = "dockerhub_id"
+
+env.registry="boomer9955/mydjango"
+env.registryCredential="dockerhub_id"
 env.dockerImage=""
 
 node{
     stage('копируем репу'){
         checkout scm
-    }
-    stage('sh'){
-        sh "groovy -v"
-        sh "pwd"
     }
     stage('собираем image'){
         curDate = readFile 'ansible/log.yml'
