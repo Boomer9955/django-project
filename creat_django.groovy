bc625bfcf7e67e75f7c2b6f8487cf628422bc298
Roman <rombich@bk.ru>
2021-06-09 17:27:07 +0300

    -

--- creat_django.groovy
+++ creat_django.groovy
@@ -1,7 +1,6 @@
 #!/usr/bin/env groovy
-//groovy.grape.Grape.grab(group:'org.springframework', module:'spring', version:'2.5.6')
-//@Grab(group='yaml', module='YamlSlurper', version='1.0.1')
-//import groovy.yaml.YamlSlurpe)
+@Grab('org.yaml:snakeyaml:1.17')
+import org.yaml.snakeyaml.Yaml
 import groovy.*
 //env.registry="boomer9955/mydjango"
 //env.registryCredential="dockerhub_id"
@@ -17,8 +16,9 @@ node{
         def ipad = (curDate =~ "\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}")
                 ip = ipad[0]
             println "${ip}"
-        def config = new YamlSlurper().parseText(curDate)
-            println "${config.user}"
+        Yaml parser = new Yaml()
+        List example = parser.load(curDate as File).text)
+        example.each{println it.user}
             //sh "ansible-vault decrypt --vault-password-file {$password} ansible/password.conf"
             //dockerImage = docker.build registry + ":latest"
     }
