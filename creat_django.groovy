23ca2c61734b0424b551f0bab57f46a7e9071fcc
Boomer9955 <73993803+Boomer9955@users.noreply.github.com>
2021-06-18 13:13:25 +0300

    Update creat_django.groovy

--- creat_django.groovy
+++ creat_django.groovy
@@ -4,10 +4,10 @@ node {
     curDate = readFile 'ansible/hosts.yml'
     def read = readYaml file: curDate
     
-    def mb = (curDate =~ "\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}")
+    /*def mb = (curDate =~ "\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}")
         ip = mb[0]
         println "${ip}"
-    sh 'curl http://${ip}:8000'
+    sh 'curl http://${ip}:8000'*/
     def amap = ['something': 'my datas',
                     'size': 3,
                     'isEmpty': false]
