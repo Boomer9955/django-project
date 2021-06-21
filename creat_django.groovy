74c0f9878b40342ceec1a42bb9ba38f870ebf762
Yurkin Roman <Yurkinrv@sberbank.ru>
2021-06-21 15:51:32 +0300

    +

--- creat_django.groovy
+++ creat_django.groovy
@@ -1,24 +1,33 @@
 #!/usr/bin/env groovy
+
 import org.yaml.snakeyaml.Yaml
 import groovy.*
 //env.registry="boomer9955/mydjango"
 //env.registryCredential="dockerhub_id"
 //env.dockerImage=""
 
-node{
-    stage('копируем репу'){
-        checkout scm
+node {
+
+    checkout scm
+
+    curDate = 'ansible/password.conf'
+
+    withCredentials([file(credentialsId: 'docker_hub', variable: SFILE)]) {
+        sh 'ansible-vault decrypt ${curDate} --vault-password-file ${SFILE}'
     }
-    stage('собираем image'){
-        curDate = readFile 'ansible/log.yml'
-        println "${curDate}"
-        def ipad = (curDate =~ "\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}")
-                ip = ipad[0]
-            println "${ip}"
-        Yaml parser = new Yaml()
-        List example = parser.load((curDate as File).text)
-        example.each{println it.user}
-            //sh "ansible-vault decrypt --vault-password-file {$password} ansible/password.conf"
-            //dockerImage = docker.build registry + ":latest"
-    }
+
+
+    //def read = readYaml file: curDate
+    
+    /*def mb = (curDate =~ "\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}")
+        ip = mb[0]
+        println "${ip}"
+    sh 'curl http://${ip}:8000'
+    def amap = ['something': 'my datas',
+                    'size': 3,
+                    'isEmpty': false]
+
+    writeYaml file: curDate, data: amap
+    def read = readYaml file: curDate
+    println "${read}"*/
 }
